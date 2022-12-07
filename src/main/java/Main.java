3bbde063ca93441930d74a21a932a7c1bea99b73
Olga Belikova <belko.belikova@gmail.com>
2022-12-07 23:29:34 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BonusService service = new BonusService();
+
+        long expected = 150;
+        long actual = service.calculate(5000, true);
+
+
+    }
+}
