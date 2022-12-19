5bf510c180aa21732174d52d2cd6503c877afc09
Olga Belikova <belko.belikova@gmail.com>
2022-12-19 21:34:45 +0300

    Add csv file

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,24 +1,21 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BonusServiceTest {
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    
+    @ParameterizedTest
+    @CsvFileSource (resources="/data.csv")
+    void shouldCalculateForRegisteredAndUnderLimit(long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
-        long amount = 1000;
-        boolean registered = true;
-        long expected = 30;
-
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
 
-    @org.junit.jupiter.api.Test
+/*    @org.junit.jupiter.api.Test
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
@@ -64,5 +61,5 @@ public class BonusServiceTest {
 
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
-    }
+    }*/
 }
